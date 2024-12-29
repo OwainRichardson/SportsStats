278d1b912719f0e01d56609500d361ec0741d86d
Owain Richardson <owain.richardson@gmail.com>
2024-12-29 21:47:41 +0000

    handle area in route

--- Angular-App/sports-stats/src/app/app.component.ts
+++ Angular-App/sports-stats/src/app/app.component.ts
@@ -1,25 +1,28 @@
 import { Component } from '@angular/core';
-import { Router, NavigationEnd, RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, NavigationEnd, RouterOutlet, RouterLink, RouterLinkActive, ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { filter, map, mergeMap } from 'rxjs/operators';
+import { SportsComponent } from "./pages/sports/sports.component";
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [ CommonModule, RouterOutlet, RouterLink, RouterLinkActive ],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, SportsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
 export class AppComponent {
   router: Router;
-  isAdmin = false;
+  area: string;
 
-  constructor(router: Router) {
+  constructor(router: Router, route: ActivatedRoute) {
     this.router = router;
 
-    router.events.subscribe((e) => {
-      if (e instanceof NavigationEnd) {
-        this.isAdmin = e.url.includes('/admin');
-      }
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.area = route.root.firstChild?.snapshot.data['area'];
+        console.log(route.root.firstChild?.snapshot.data['area']);
     });
   }
 
--- Angular-App/sports-stats/src/app/app.routes.ts
+++ Angular-App/sports-stats/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { SportsComponent } from './pages/sports/sports.component';
 import { SportComponent } from './pages/sport/sport.component';
 
 export const routes: Routes = [
-    { path: 'sports', component: SportsComponent, title: 'Home' },
-    { path: 'sports/:sportId', component: SportComponent, title: 'Sport' },
-    { path: '**', component: SportsComponent, title: 'Home' },
-];
+    { path: 'sports', component: SportsComponent, title: 'Home', data: { area: 'geese'} },
+    { path: 'sports/:sportId', component: SportComponent, title: 'Sport', data: { area: 'sport'}  },
+    { path: '**', component: SportsComponent, title: 'Home', data: { area: 'main'}  },
+]; 
