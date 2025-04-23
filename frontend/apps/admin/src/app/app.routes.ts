cb45a66e2026f2dd11bba8c3b4b9ab852d54460b
Owain Richardson <owain.richardson@gmail.com>
2025-04-23 21:33:04 +0100

    Update admin app

--- frontend/apps/admin/src/app/app.routes.ts
+++ frontend/apps/admin/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Route } from '@angular/router';
+import { HomeComponent } from './routes/home/home.component';
+import { SportsComponent } from './routes/sports/sports.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    { path: '', component: HomeComponent, pathMatch: 'full' },
+    { path: 'sports', component: SportsComponent, pathMatch: 'full' },
+];
---
+++ frontend/apps/admin/src/app/routes/sports/sports.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sports',
+  imports: [CommonModule],
+  templateUrl: './sports.component.html',
+  styleUrl: './sports.component.css',
+})
+export class SportsComponent {}
